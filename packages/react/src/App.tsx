d0b53f4e7ef4aa7837704f899c064395b2df75d4
sinedied <noda@free.fr>
2023-12-13 17:07:55 +0100

    feat: add React integration example

---
+++ packages/react/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import '@chatbootai/chat';
+
+function App() {
+  return (
+    <>
+      <nav>ChatBootAI</nav>
+      <cba-chat></cba-chat>
+    </>
+  )
+}
+
+export default App
