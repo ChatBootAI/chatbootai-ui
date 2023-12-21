5b9149a56551be3b33dcfec6b2067bdd24e7bd81
sinedied <noda@free.fr>
2023-12-21 17:25:57 +0100

    feat: add svelte example integration

---
+++ packages/svelte/src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import '@chatbootai/chat';
+</script>
+
+<main>
+  <nav>ChatBootAI</nav>
+  <cba-chat></cba-chat>
+</main>
+
+<style>
+  nav {
+    background: #333;
+    color: #fff;
+    padding: 16px;
+    font-family:
+      'Segoe UI',
+      -apple-system,
+      BlinkMacSystemFont,
+      Roboto,
+      'Helvetica Neue',
+      sans-serif;
+    font-size: 1.25rem;
+  }
+  cba-chat {
+    display: block;
+    max-width: 1024px;
+    margin: 0 auto;
+  }
+</style>
